1d10bac98985f93c6c18f516db526490e111fb8a
Viktor Abrosimov <vitya.abrosimoff2016@yandex.ru>
2021-09-24 21:50:29 +0500

    feat(arrWithAssortmentOfClub): добавить инфу для компонента clubMenu

---
+++ src/utils/arrWithAssortmentOfClub.js
@@ -0,0 +1,144 @@
+export const arrWithAssortmentOfClub = [
+  {
+    title: "Фирменные кальяны",
+    assortments: [
+      {
+        name: "Баунти",
+        price: "3000",
+        supplements: "шоколад, молоко, кокосовый сироп",
+        bowl: "ананас",
+        strength: "средняя",
+      },
+      {
+        name: "Мохито",
+        price: "3000",
+        supplements: "мята, лайм, белый ром и много-много льда",
+        bowl: "яблоко, апельсин, грейпфрут",
+        strength: "крепкий",
+      },
+      {
+        name: "Пина колада",
+        price: "3000",
+        supplements: "молоко, ананасовый сок, кокосовый сироп, лед",
+        bowl: "ананас",
+        strength: "средняя",
+      },
+    ],
+  },
+  {
+    title: "Сигары и сигарилы",
+    assortments: [
+      {
+        name: "Куба",
+        brands: ["Cohiba Panatelas", "Cohiba Siglo II", "Cohiba Siglo IV", "Cohiba Siglo V", "Cohiba Robustos", "H. Upmann Magnum", "Hoyo De Mont. Epicure №1", "Montecristo №4", "Partagas Serie №4", "Romeo & Julieta Churchliss", "Romeo & Julieta №2"],
+      },
+      {
+        name: "Доминикана",
+        brands: ["CT Reserva Decadencia Churchill", "Arturo Fuente Anejo №77 Shark", "Davidoff Escurio Gran Perfecto", "Davidoff WSC Churchill", "Zino Platinum XS", "Zino Platinum Barrel", "Macanudo Cafe Ascots", "Macanudo Maduro Ascots"],
+      },
+      {
+        name: "Никарагуа",
+        brands: ["Nub Sun Grown 460 Tubos"],
+      },
+    ],
+  },
+  {
+    title: "Классические кальяны",
+    assortments: [
+      {
+        name: "Кальяны на глиняной чаше",
+        views: [
+          {
+            type: "",
+            price: "",
+            strength: "",
+          },
+          {
+            type: "",
+            price: "",
+            strength: "",
+          },
+        ],
+        bowlChange: [
+          {
+            type: "",
+            price: "",
+          },
+          {
+            type: "",
+            price: "",
+          },
+        ],
+      },
+      {
+        name: "Кальяны на фруктовой чаше",
+        views: [
+          {
+            hookahs: [
+              {
+                fruit: "Апельсин",
+                type: [
+                  {
+                    classification: "Standart:",
+                    price: "1600",
+                    strength: "",
+                  },
+                  {
+                    classification: "Premium:",
+                    price: "1900",
+                    strength: "",
+                  },
+                ]
+              },
+              {
+                fruit: "Яблоко",
+                type: [
+                  {
+                    classification: "Standart:",
+                    price: "1600",
+                    strength: "",
+                  },
+                  {
+                    classification: "Premium:",
+                    price: "1900",
+                    strength: "",
+                  },
+                ]
+              },
+              {
+                fruit: "Грейпфрут",
+                type: [
+                  {
+                    classification: "Standart:",
+                    price: "1900",
+                    strength: "",
+                  },
+                  {
+                    classification: "Premium:",
+                    price: "2300",
+                    strength: "",
+                  },
+                ]
+              },
+              {
+                fruit: "Ананас",
+                type: [
+                  {
+                    classification: "Standart:",
+                    price: "2200",
+                    strength: "",
+                  },
+                  {
+                    classification: "Premium:",
+                    price: "2700",
+                    strength: "",
+                  },
+                ]
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  }
+];
