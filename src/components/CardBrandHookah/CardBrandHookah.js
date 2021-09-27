b31bdddb1d92217bb23b52f9e0906d2185786649
v1ktorbro <vitya.abrosimoff2016@ya.ru>
2021-09-27 15:39:20 +0500

    feat(CardBrandHookah): верстка карточки для фирменных кальянов

---
+++ src/components/CardBrandHookah/CardBrandHookah.js
@@ -0,0 +1,23 @@
+import './CardBrandHookah.css';
+
+function CardBrandHookah({dataCard}) {
+  return (
+    <article className="card-brand-hookah">
+      <div className="card-brand-hookah__block-title">
+        <h3 className="card-brand-hookah__title">&#171;{dataCard.name}&#187;</h3>
+        <span className="card-brand-hookah__price">{dataCard.price}&#32;&#8381;</span>
+      </div>
+        <h4 className="card-brand-hookah__title-ingredient">Добавки:
+          <span className="card-brand-hookah__kind-ingredient">&#32;{dataCard.supplements}</span>
+        </h4>
+        <h4 className="card-brand-hookah__title-ingredient">{dataCard.bowl.length > 1 ? "Чаша на выбор" : "Чаша"}:
+          <span className="card-brand-hookah__kind-ingredient">&#32;{dataCard.bowl.join(', ')}</span>
+        </h4>
+        <h4 className="card-brand-hookah__title-ingredient">Крепость:
+          <span className="card-brand-hookah__kind-ingredient">&#32;{dataCard.strength}</span>
+        </h4>
+    </article>
+  );
+}
+
+export default CardBrandHookah;
