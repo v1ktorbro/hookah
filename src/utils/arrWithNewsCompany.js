1269adea941a50e6860943ce642c530ba03c7a39
v1ktorbro <vitya.abrosimoff2016@ya.ru>
2021-10-04 18:49:20 +0500

    feat(arrWithNewsCompany): массив с инфой для компонента News

---
+++ src/utils/arrWithNewsCompany.js
@@ -0,0 +1,32 @@
+export const arrWithNewsCompany = [
+  {
+    srcImage: require('../images/image_card_news_1.png').default,
+    dataNews: '24 марта',
+    title: 'Наш курительный клуб ежедневно работает с 16:00'
+  },
+  {
+    srcImage: require('../images/image_card_news_2.png').default,
+    dataNews: '8 апреля',
+    title: 'Дорогие гости, по выходным наш курительный клуб работает до 4 утра'
+  },
+  {
+    srcImage: require('../images/image_card_news_3.png').default,
+    dataNews: '26 марта',
+    title: 'Готовимся к вечеру с сигарами Davidoff'
+  },
+  {
+    srcImage: require('../images/interior_1.png').default,
+    dataNews: '12 марта',
+    title: 'Наш курительный клуб ежедневно работает с 16:00'
+  },
+  {
+    srcImage: require('../images/interior_2.png').default,
+    dataNews: '24 марта',
+    title: 'Дорогие гости, по выходным наш курительный клуб работает до 4 утра'
+  },
+  {
+    srcImage: require('../images/interior_3.png').default,
+    dataNews: '8 апреля',
+    title: 'Готовимся к вечеру с сигарами Davidoff'
+  },
+];
