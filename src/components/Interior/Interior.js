c1e9a987451c468006cc89d8b44fb8a405e3b6a5
v1ktorbro <vitya.abrosimoff2016@ya.ru>
2021-10-10 23:51:37 +0500

    refacotr(Interior): импортировать children не через  app

--- src/components/Interior/Interior.js
+++ src/components/Interior/Interior.js
@@ -1,15 +1,24 @@
 import './Interior.css';
+import React, { useState } from 'react';
 import {arrWithInteriorImages} from '../../utils/arrWithInteriorImages';
+import NavSwitchesCards from '../NavSwitchesCards/NavSwitchesCards';
 
 function Interior({children}) {
+  //массив с карточками будет приходить из NavSwitchesCards
+  const [arrOnRender, setArrOnRender] = useState([]);
   return (
     <section className="interior">
       <div className="interior__block-navigation">
         <h2 className="interior__title">Интерьер</h2>
-        {children}
+        <NavSwitchesCards
+          style={{color: '#D5D9E3'}}
+          arrWIthCards={arrWithInteriorImages}
+          countCardOnPage={4}
+          setArrOnRender={setArrOnRender}
+        />
       </div>
       <div className="interior__block-image">
-        {arrWithInteriorImages.map((el, i) => {
+        {arrOnRender.map((el, i) => {
           return (
             <img src={el.src} alt={`фото интерьера ${i + 1}`} className="interior__image" key={i} />
           );
