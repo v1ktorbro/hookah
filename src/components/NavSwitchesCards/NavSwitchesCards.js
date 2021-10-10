952395d6e6e6f30402b153ae696cff109f6035fe
v1ktorbro <vitya.abrosimoff2016@ya.ru>
2021-10-10 23:52:38 +0500

    fix(NavSwitchesCards): реализация функциональности компонента

--- src/components/NavSwitchesCards/NavSwitchesCards.js
+++ src/components/NavSwitchesCards/NavSwitchesCards.js
@@ -1,12 +1,43 @@
 import './NavSwitchesCards.css';
+import React, { useEffect, useReducer} from 'react';
 /* кнопки наследуют цвет компонента (через пропс style) */
 
-function NavSwitchesCards({style, btnPrevImage, btnNextImage, idBtn}) {
-  
+function NavSwitchesCards({style, idBtn, countCardOnPage, arrWIthCards, setArrOnRender}) {
+  const [state, dispatch] = useReducer(handlerBtnClick, { startIndex: 0, lastIndex: countCardOnPage});
+  //колбэк ф-я для useReducer
+  function handlerBtnClick(state, btnClick) {
+    switch (btnClick) {
+      case '>': return { startIndex: state.startIndex + 1, lastIndex: state.lastIndex + 1};
+      case '<': return { startIndex: state.startIndex - 1, lastIndex: state.lastIndex - 1};
+      default: return state;
+    }
+  }
+
+  function handlerNewSliceArr (evt) {
+    dispatch(evt.target.textContent)
+    setArrOnRender(arrWIthCards.slice(state.startIndex, state.lastIndex));
+  }
+
+  useEffect(() => {
+    setArrOnRender(arrWIthCards.slice(state.startIndex, state.lastIndex));
+  }, []);
+
   return (
     <nav className="nav-switches-cards" style={style}>
-      <button onClick={(evt) => btnPrevImage(evt)} className="nav-switches-cards__button" id={`btn-${idBtn}-prev`}>&#60;</button>
-      <button onClick={(evt) => btnNextImage(evt)} className="nav-switches-cards__button" id={`btn-${idBtn}-next`}>&#62;</button>
+      <button  
+        className={`nav-switches-cards__button ${state.startIndex === 0 ? "nav-switches-cards__button_disabled-style" : ""}`}
+        id={`btn-${idBtn}-prev`}
+        disabled={state.startIndex === 0 ? true : false} 
+        onClick={(evt) => handlerNewSliceArr(evt)}
+        >&#60;
+      </button>
+      <button 
+        className={`nav-switches-cards__button ${state.lastIndex === arrWIthCards.length ? "nav-switches-cards__button_disabled-style" : ""}`}
+        id={`btn-${idBtn}-next`}
+        disabled={state.lastIndex === arrWIthCards.length ? true : false} 
+        onClick={(evt) => handlerNewSliceArr(evt)} 
+        >&#62;
+      </button>
     </nav>
   );
 }
